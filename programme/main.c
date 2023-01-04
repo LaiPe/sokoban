f391f5a0ab034cc50d35999f2fae6220bb73f2c2
Léo Desktop <peyronnet.leo@gmail.com>
2023-01-04 13:58:06 +0100

    jeu jouable (normalement), caisses et cibles implémentées

--- programme/main.c
+++ programme/main.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
+/*le mouvement haut est bon*/
+
 #define LIGNS 10
 #define COLS 10
 
 char terrain[LIGNS][COLS] = {
     '#', '#', '#', '#', '#', '#', '#', '#', '#', '#',
     '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#',
-    '#', ' ', '#', '#', '#', '#', '#', '#', ' ', '#',
-    '#', ' ', '#', ' ', ' ', ' ', ' ', '#', ' ', '#',
-    '#', ' ', '#', '#', '#', '#', '#', '#', ' ', '#',
+    '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#',
+    '#', ' ', ' ', ' ', ' ', 'x', ' ', ' ', ' ', '#',
+    '#', ' ', ' ', ' ', ' ', '0', ' ', ' ', ' ', '#',
+    '#', ' ', ' ', ' ', ' ', 'P', ' ', ' ', ' ', '#',
+    '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#',
     '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#',
     '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'
 };
 
-int persoLign = 1;
-int persoCol = 1;
+//Variables de position du personage
+int persoLign= 5;
+int persoCol= 5;
+short int persoSurCible = 0;
+
+//Variables de déplacement du personage
+int x;
+int y;
+
+int obstacle(char a){
+    return a=='#' || a=='0' || a=='O';
+}
 
 void printTerrain() {
     for (int lign = 0; lign < LIGNS; lign++) {
@@ -34,36 +48,96 @@ int main() {
         printf("Déplacement ? (g/h/d/b): ");
         scanf("%s", input);
 
-        if (strcmp(input, "h") == 0) {
-            // Déplacement haut
-            if (terrain[persoLign - 1][persoCol] != '#') {
-                terrain[persoLign][persoCol] = ' ';
-                persoLign--;
-                terrain[persoLign][persoCol] = 'P';
-            }
-        } else if (strcmp(input, "b") == 0) {
-            // Déplacement bas
-            if (terrain[persoLign + 1][persoCol] != '#') {
-                terrain[persoLign][persoCol] = ' ';
-                persoLign++;
-                terrain[persoLign][persoCol] = 'P';
-            }
-        } else if (strcmp(input, "g") == 0) {
-            // Déplacement gauche
-            if (terrain[persoLign][persoCol - 1] != '#') {
-                terrain[persoLign][persoCol] = ' ';
-                persoCol--;
-                terrain[persoLign][persoCol] = 'P';
-            }
-        } else if (strcmp(input, "d") == 0) {
-            // Déplacement droit
-            if (terrain[persoLign][persoCol + 1] != '#') {
-                terrain[persoLign][persoCol] = ' ';
-                persoCol++;
-                terrain[persoLign][persoCol] = 'P';
+        //Valeurs en fonction du déplacement idiqué par l'utilisateur
+        if (strcmp(input, "h") == 0) {y=-1;x=0;}
+        else if (strcmp(input, "b") == 0) {y=1;x=0;} 
+        else if (strcmp(input, "g") == 0) {y=0;x=-1;} 
+        else if (strcmp(input, "d") == 0) {y=0;x=1;}
+        else {y=0;x=0;}
+
+        // Regles déplacement
+        if (terrain[persoLign + y][persoCol + x] == ' ') {
+            // Vers une case vide
+            terrain[persoLign][persoCol] = ' ';
+            persoLign += y;
+            persoCol += x;
+            terrain[persoLign][persoCol] = 'P';
+
+            //De plus: si le perso se trouve sur une cible
+            if (persoSurCible){
+                terrain[persoLign - y][persoCol - x] = 'x';
+                persoSurCible--;
             }
         }
-        system("clear");
+        else if (terrain[persoLign + y][persoCol + x] == 'O' && !(obstacle(terrain[persoLign + 2*y][persoCol + 2*x]))){
+            // Vers une caisse à pousser sans obstacle derrière
+            if (terrain[persoLign + 2*y][persoCol + 2*x] == 'x'){
+                // S'il y a une cible derrière la caisse
+                terrain[persoLign][persoCol] = ' ';
+                persoLign += y;
+                persoCol += x;
+                terrain[persoLign][persoCol] = 'P';
+                terrain[persoLign + y][persoCol + x] = '0'; 
+            }
+            else{
+                terrain[persoLign][persoCol] = ' ';
+                persoLign += y;
+                persoCol += x;
+                terrain[persoLign][persoCol] = 'P';
+                terrain[persoLign + y][persoCol + x] = 'O';
+            }
+
+            //De plus: si le perso se trouve sur une cible
+            if (persoSurCible){
+                terrain[persoLign - y][persoCol - x] = 'x';
+                persoSurCible--;
+            }
+        }
+        else if (terrain[persoLign + y][persoCol + x] == '0' && !(obstacle(terrain[persoLign + 2*y][persoCol + 2*x]))){
+            // Vers une caisse sur une cible (sans obstacle derrière)
+            if (terrain[persoLign + 2*y][persoCol + 2*x] == 'x'){
+                // S'il y a une cible derrière la caisse
+                terrain[persoLign][persoCol] = ' ';
+                persoLign += y;
+                persoCol += x;
+                terrain[persoLign][persoCol] = 'P';
+                terrain[persoLign + y][persoCol + x] = '0';
+
+                //De plus: si le perso se trouve sur une cible
+                if (persoSurCible){
+                    terrain[persoLign - y][persoCol - x] = 'x';
+                    persoSurCible--;
+                }
+                persoSurCible++;
+            }
+            else{
+                terrain[persoLign][persoCol] = ' ';
+                persoLign += y;
+                persoCol += x;
+                terrain[persoLign][persoCol] = 'P';
+                terrain[persoLign + y][persoCol + x] = 'O';
+
+                //De plus: si le perso se trouve sur une cible
+                if (persoSurCible){
+                    terrain[persoLign - y][persoCol - x] = 'x';
+                    persoSurCible--;
+                }
+                persoSurCible++;
+            }
+        }
+        else if (terrain[persoLign + y][persoCol + x] == 'x') {
+            // Vers une cible
+            terrain[persoLign][persoCol] = ' ';
+            persoLign += y;
+            persoCol += x;
+            terrain[persoLign][persoCol] = 'P';
+            //De plus: si le perso se trouve sur une cible
+            if (persoSurCible){
+                terrain[persoLign - y][persoCol - x] = 'x';
+                persoSurCible--;
+            }
+            persoSurCible++;
+        }
         printTerrain();
     }
 }
